1bf8f815398a6a052fe9b05509f0824055ad1e3b
Kevin Catlett <kevin.catlett@terovania.de>
2024-11-19 12:51:42 +0100

    Grid and shader polish, random AI colors

--- Assets/Scripts/AI/RandomizeCharacter.cs
+++ Assets/Scripts/AI/RandomizeCharacter.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class RandomizeCharacter : MonoBehaviour
 {
     [SerializeField] private GameObject[] characters;
-    
+    [SerializeField] private GameObject[] characterMeshes;
+    [SerializeField] private int[] headIndexes;
+    [SerializeField] private int[] clothIndexes;
     private void Awake()
     {
         foreach (GameObject character in characters)
@@ -16,5 +18,10 @@ public class RandomizeCharacter : MonoBehaviour
         int randomIndex = UnityEngine.Random.Range(0, characters.Length);
         
         characters[randomIndex].SetActive(true);
+
+        int randomColor = UnityEngine.Random.Range(0, 9);
+        characterMeshes[randomIndex].GetComponent<SkinnedMeshRenderer>().materials[headIndexes[randomIndex]].SetFloat("_Hue", randomColor);
+        characterMeshes[randomIndex].GetComponent<SkinnedMeshRenderer>().materials[clothIndexes[randomIndex]].SetFloat("_Hue", randomColor);
+
     }
 }

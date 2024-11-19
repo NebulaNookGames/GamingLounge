af8845cc25ac64ae02b2b79ce3afb2c10120c0e7
Kevin Catlett <kevin.catlett@terovania.de>
2024-11-19 09:18:00 +0100

    Changed the character objects and implemented the idle animation for each

--- Assets/Scripts/AI/RandomizeCharacter.cs
+++ Assets/Scripts/AI/RandomizeCharacter.cs
@@ -4,42 +4,17 @@ using UnityEngine;
 
 public class RandomizeCharacter : MonoBehaviour
 {
-    [SerializeField] private bool doIt = false; 
-    [SerializeField] private GameObject[] bodies;
-    [SerializeField] private GameObject[] heads;
-    [SerializeField] private GameObject headMesh;
-    private int materialIndex; 
+    [SerializeField] private GameObject[] characters;
+    
     private void Awake()
     {
-        if (!doIt) return; 
-        
-        foreach (GameObject body in bodies)
+        foreach (GameObject character in characters)
         {
-            body.SetActive(false);
-        }
-
-        foreach (GameObject head in heads)
-        {
-            head.SetActive(false);
+            character.SetActive(false);
         }
         
-        int randomIndex = UnityEngine.Random.Range(0, bodies.Length);
+        int randomIndex = UnityEngine.Random.Range(0, characters.Length);
         
-        bodies[randomIndex].SetActive(true);
-        heads[randomIndex].SetActive(true);
-        // switch (randomIndex) 
-        // {
-        //     case 0:
-        //         materialIndex = 0;
-        //         break;
-        //     case 1:
-        //         materialIndex = 3;
-        //         break;
-        //     case 2:
-        //         materialIndex = 0;
-        //         break;
-        // }
-        // float randomColorIndex = UnityEngine.Random.Range(0, 1);
-        // headMesh.GetComponent<MeshRenderer>().materials[materialIndex].SetFloat("_Hue", randomColorIndex);
+        characters[randomIndex].SetActive(true);
     }
 }

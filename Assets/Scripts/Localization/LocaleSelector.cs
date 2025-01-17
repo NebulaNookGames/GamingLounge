245948af86973f2723a76f63d26c16509684a3e9
Kevin Catlett <kevin.catlett@terovania.de>
2025-01-17 14:53:42 +0100

    Added spawn animations for structural objects and the first variant of a options menu

--- Assets/Scripts/Localization/LocaleSelector.cs
+++ Assets/Scripts/Localization/LocaleSelector.cs
@@ -1,16 +1,21 @@
+using System;
 using UnityEngine;
 using System.Collections;
 using UnityEngine.Localization.Settings;
+using TMPro; 
 
 public class LocaleSelector : MonoBehaviour
 {
-    private bool active = false; 
+    private bool active = false;
+    public TMP_Dropdown languageDropdown;
     
     IEnumerator SetLocale(int localeID)
     {
         active = true; 
+        int currentSetLocale = languageDropdown.value;
+        
         yield return LocalizationSettings.InitializationOperation;
-        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[localeID];
+        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[currentSetLocale];
         active = false; 
     }
 
---
+++ Assets/Scripts/UI/OptionsHandler.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+public class OptionsHandler : MonoBehaviour
+{
+    public GameObject settingsUIObjects;
+    public GameObject mainUIObjects; 
+    public AudioMixer audioMixer;
+    private float initialVolume = -20f;
+    public Toggle fullscreenToggle;
+    [FormerlySerializedAs("volumeToggle")] public Toggle musicVolumeToggle; 
+    
+    public void ChangeScreenMode(bool value)
+    {
+        if(fullscreenToggle.isOn)
+            Screen.fullScreen = true;
+        else 
+            Screen.fullScreen = false;
+    }
+
+    public void ToggleSettingsMenu()
+    {
+        if (settingsUIObjects.activeSelf)
+        {
+            settingsUIObjects.SetActive(false);
+            mainUIObjects.SetActive(true);
+        }
+        else
+        {
+            settingsUIObjects.SetActive(true);
+            mainUIObjects.SetActive(false);
+        }
+    }
+
+    public void ChangeMusic(bool value)
+    {
+        Debug.Log("Changing Music volume");
+        
+        if(musicVolumeToggle.isOn)
+            UnmuteGroup();
+        else
+            MuteGroup();
+    }
+    
+    public void UnmuteGroup()
+    {
+        Debug.Log("unmuting");
+        audioMixer.SetFloat("Music", -20f);
+    }
+    
+    public void MuteGroup()
+    {
+        Debug.Log("muting");
+        audioMixer.SetFloat("Music", -80);
+    }
+
+}
